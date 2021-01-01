f0472d54de3626b0b2ca03f3377bb280dcc8a1a2
maxRN <max12.gro@gmail.com>
2021-01-01 21:35:54 +0100

    alice, carlos hinzugefügt; bob verändert
    Bob kann genutzt werden um die Modulregel zu testen.
    
    Alice ist der krasseste Student ever und muss nur noch die Bachelorprüfung absolvieren.
    
    Carlos hat noch nicht alle Prüfungen abgelegt, aber kann theoretisch immernoch die Bachelorprüfung absolvieren.

--- studenten.pl
+++ studenten.pl
@@ -1,4 +1,46 @@
 /* abgelegt/4 (name, pruefung, note, versuch) */
+
+/* bob hat 3 wiederholungen für rechnernetze gebraucht und ist bei datenbanken 2 mal durchgefallen */
+/* laut der speziellen Regel sollte er als exmatrikuliert gewertet werden */
+/* (Obwohl er eigentlich noch einen Versuch frei hätte.) */
+
 abgelegt(bob, logik, 1.0, 1).
 abgelegt(bob, mathe, 1.3, 1).
 abgelegt(bob, rechnernetze, 5.0, 1).
+abgelegt(bob, rechnernetze, 5.0, 2).
+abgelegt(bob, rechnernetze, 4.0, 3).
+abgelegt(bob, datenbanken, 5.0, 1).
+abgelegt(bob, datenbanken, 5.0, 2).
+
+
+/* Alice ist ein turbo smurf und hat alle Pruefungen beim 1. Versuch bestanden. */
+/* Ihr fehlt nur noch die Bachelorprüfung. */
+
+abgelegt(alice, einfuehrungspraktikum, 1.0, 1).
+abgelegt(alice, mathe_1_1, 1.0, 1).
+abgelegt(alice, mathe_1_2, 1.0, 1).
+abgelegt(alice, mathe_2_1, 1.0, 1).
+abgelegt(alice, mathe_2_2, 1.0, 1).
+abgelegt(alice, aud, 1.0, 1).
+abgelegt(alice, programmierung, 1.0, 1).
+abgelegt(alice, ikt, 1.0, 1).
+abgelegt(alice, fs, 1.0, 1).
+abgelegt(alice, logik, 1.0, 1).
+abgelegt(alice, swt, 1.0, 1).
+abgelegt(alice, swp, 1.0, 1).
+abgelegt(alice, rechnerarchitektur, 1.0, 1).
+abgelegt(alice, datenbanken, 1.0, 1).
+abgelegt(alice, rechnernetze, 1.0, 1).
+abgelegt(alice, bus, 1.0, 1).
+abgelegt(alice, tgi, 1.0, 1).
+abgelegt(alice, soi, 1.0, 1).
+abgelegt(alice, is, 1.0, 1).
+abgelegt(alice, emi, 1.0, 1).
+abgelegt(alice, ecg, 1.0, 1).
+abgelegt(alice, vertiefung, 1.0, 1).
+abgelegt(alice, vertiefung_bachelor, 1.0, 1).
+abgelegt(alice, aqua, 1.0, 1).
+
+/* Carlos hat noch nicht alle Prüfungen absolviert, ist aber theoretisch noch in der Lage die Bachelorprüfung abzulegen. */
+
+abgelegt(carlos, einfuehrungspraktikum, 1.0, 1).
